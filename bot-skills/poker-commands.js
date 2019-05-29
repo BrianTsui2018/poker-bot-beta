463453920085123fcc5ae4cd1d634673cc64f8a0
stephclleung <35399643+stephclleung@users.noreply.github.com>
2019-05-29 18:42:15 +0800

    Adding try catch for placeBet

--- bot-skills/poker-commands.js
+++ bot-skills/poker-commands.js
@@ -415,11 +415,16 @@ const placeBet = async (data) => {
             userID: data.user_slack_id
         }
 
-        let body = await axiosPUT(betData);
+        try {
+            let body = await axiosPUT(betData);
+            data.spent = body.action;
+            await updatePlayerWallet(data);
+        } catch (error) {
+            console.log("poker-command.js | Place bet error. | Returning ing 0")
+            console.log(error);
+            return 0;
+        }
 
-        data.spent = body.action;
-
-        await updatePlayerWallet(data);
         return body.data;
     }
     else {
